243f7159761b7bfc235565741000c4582be0e3ac
desertsniper87 <torshobuet@gmail.com>
2020-09-27 17:28:28 +0600

    implement template

--- src/main/java/com/desertsniper/github/library/LibraryApplication.java
+++ src/main/java/com/desertsniper/github/library/LibraryApplication.java
@@ -2,10 +2,17 @@ package com.desertsniper.github.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class LibraryApplication {
+public class LibraryApplication extends WebMvcConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "/readingList");
 	}
 }
---
+++ src/main/java/com/desertsniper/github/library/controller/ReadingListController.java
@@ -0,0 +1,46 @@
+package com.desertsniper.github.library.controller;
+
+import com.desertsniper.github.library.ReadingListRepository;
+import com.desertsniper.github.library.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class ReadingListController {
+
+    private final ReadingListRepository readingListRepository;
+
+    @Autowired
+    public ReadingListController(ReadingListRepository readingListRepository) {
+        this.readingListRepository = readingListRepository;
+    }
+
+    @RequestMapping(value="/{reader}", method=RequestMethod.GET)
+    public String readersBooks(
+            @PathVariable("reader") String reader,
+            Model model) {
+
+        List<Book> readingList =
+                readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+        }
+        return "readingList";
+    }
+
+    @RequestMapping(value="/{reader}", method=RequestMethod.POST)
+    public String addToReadingList(
+            @PathVariable("reader") String reader, Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/{reader}";
+    }
+
+}
