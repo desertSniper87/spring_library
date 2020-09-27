443fa84e3172fbe78d240d0ce3767ec066f2fa7f
desertsniper87 <torshobuet@gmail.com>
2020-09-27 12:03:31 +0600

    spring initialize

---
+++ src/test/java/com/desertsniper/github/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desertsniper.github.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
